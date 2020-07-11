ecbefc1b33d969cf2477a15eebd9f4eb1979c880
naveen <68142314+naveenkrish1@users.noreply.github.com>
2020-07-11 20:26:28 +0530

    Add files via upload

---
+++ mysqll.sql
@@ -0,0 +1,6 @@
+create database mydb;
+use mydb
+CREATE TABLE USER(username char(50) NOT NULL ,password char(50) NOT NULL);
+create table NOTIFICATIONS(username char(50),notification char(50),status char(50) not null);
+create REGISTEREDCOURSES(username char(50) NOT NULL,courseid char(50) not null,coursename char(50))
+create table COURSE(courseid char(50) not null,coursename char(50))
